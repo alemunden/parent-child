ddbb8239ffdc0abc3216c7838847898c87615262
ale munden <amunden@my.bcit.ca>
2024-03-25 10:25:35 -0700

    initial commit

--- .history/pages/index_20240325095301.js
+++ .history/pages/index_20240325095301.js
@@ -1,13 +1,10 @@
 import styles from '@/styles/Home.module.css'
-import ParentToChild from './ParentOne'
-import ChildToParent from './ParentTwo'
 
 export default function Home() {
   return (
     <>
       <main className={`${styles.main}`}>
-        <ParentToChild/>
-        <ChildToParent/>
+       
       </main>
     </>
   )
